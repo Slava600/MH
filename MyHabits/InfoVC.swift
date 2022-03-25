7b8e7fccf1edb03781b62c51cfb23ed0c84e03ed
Slava600 <Slava600@yandex.ru>
2022-03-25 19:31:30 +0300

    Добавлен tapBar  с двумя контроллерами HabitsVC, InfiVC

---
+++ MyHabits/InfoVC.swift
@@ -0,0 +1,29 @@
+//
+//  InfoVC.swift
+//  MyHabits
+//
+//  Created by Архипов Вячеслав on 25.03.2022.
+//
+
+import UIKit
+
+class InfoVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- MyHabits/SceneDelegate.swift
+++ MyHabits/SceneDelegate.swift
@@ -9,16 +9,52 @@ import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
-    var window: UIWindow?
+    internal var window: UIWindow?
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let scene = (scene as? UIWindowScene) else { return }
+        let window = UIWindow(windowScene: scene)
+        
+        
+        let habitsVC = HabitsVC()
+        habitsVC.view.backgroundColor = UIColor.white
+        let habitsNavVC = UINavigationController(rootViewController: habitsVC)
+        habitsNavVC.tabBarItem = UITabBarItem(title: "Привычки", image: UIImage(named: "tabIconHabits"), selectedImage: UIImage(named: "tabIconHabits"))
+        habitsNavVC.navigationBar.barTintColor = UIColor.brown
+
+       
+        let infoVC = InfoVC()
+        infoVC.view.backgroundColor = UIColor.white
+        let infoNavVC = UINavigationController(rootViewController: infoVC)
+        infoNavVC.tabBarItem = UITabBarItem(title: "Информация", image: UIImage(systemName: "info.circle"), selectedImage: UIImage(systemName: "info.circle"))
+        
+        
+        
+        
+        let tabBC = UITabBarController()
+        tabBC.tabBar.backgroundColor = UIColor(named: "LightGray")
+        tabBC.viewControllers = [habitsNavVC, infoNavVC]
+        
+        
+        
+        
+        
+        
+        window.rootViewController = tabBC
+        window.makeKeyAndVisible()
+        
+        self.window = window
     }
 
+    
+    
+    
+    
+    
+    
+    
+    
     func sceneDidDisconnect(_ scene: UIScene) {
         // Called as the scene is being released by the system.
         // This occurs shortly after the scene enters the background, or when its session is discarded.
