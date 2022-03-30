1523939728c46b04f0ef13e618390bfa22e0ff6b
Slava600 <Slava600@yandex.ru>
2022-03-31 00:34:37 +0300

    добавлены: Лейблы: Время, Каждый день в..., установленное время, Пикер времени, кнопка отменить

--- MyHabits/Habits/HabitVC.swift
+++ MyHabits/Habits/HabitVC.swift
@@ -11,23 +11,7 @@ class HabitVC: UIViewController {
     
     var habit:Habit?
     var habitName = ""
-    var habitTime = Date(){
-        didSet{
-            let dateformat = DateFormatter()
-            dateformat.dateFormat = "HH:mm a"
-        }
-    }
     
-    
-    var habitColor: UIColor = .systemBlue {
-        didSet {
-            habitNameText.textColor = habitColor
-//            dateValue.textColor = habitColor
-            colorButton.backgroundColor = habitColor
-        }
-    }
-    
-
     lazy var habitNameLabel: UILabel = {
         let label = UILabel()
         label.font = UIFont.systemFont(ofSize: 17, weight: .semibold)
@@ -37,6 +21,14 @@ class HabitVC: UIViewController {
         label.text = "Название"
         return label
     }()
+    
+    var habitColor: UIColor = .systemBlue {
+        didSet {
+            habitNameText.textColor = habitColor
+            setTimeValue.textColor = habitColor
+            colorButton.backgroundColor = habitColor
+        }
+    }
     
     lazy var habitNameText: UITextField = {
         let text = UITextField()
@@ -75,11 +67,94 @@ class HabitVC: UIViewController {
     
     @objc func toColorButton(){
         let colorPicker = UIColorPickerViewController()
-        
+        colorPicker.selectedColor = habitColor
+        colorPicker.delegate = self
+        present(colorPicker, animated: true, completion: nil)
     }
     
+    lazy var timeLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 17, weight: .semibold)
+        label.toAutoLayout()
+        label.textColor = .black
+        label.numberOfLines = 1
+        label.text = "Время"
+        return label
+    }()
     
-
+    lazy var everyDayInLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 17, weight: .semibold)
+        label.toAutoLayout()
+        label.textColor = .black
+        label.numberOfLines = 1
+        label.text = "Каждый день в "
+        return label
+    }()
+    
+    var habitTime = Date(){
+        didSet{
+            let dateformat = DateFormatter()
+            dateformat.dateFormat = "HH:mm a"
+            setTimeValue.text = dateformat.string(from: habitTime)
+        }
+    }
+    
+    lazy var setTimeValue: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 17, weight: .semibold)
+        label.toAutoLayout()
+        label.textColor = habitColor
+        label.numberOfLines = 1
+        let dateformat = DateFormatter()
+        dateformat.dateFormat = "HH:mm a"
+        label.text = dateformat.string(from: habitTime)
+        return label
+    }()
+    
+    lazy var timePicker: UIDatePicker = {
+        let timePicker = UIDatePicker()
+        timePicker.toAutoLayout()
+        timePicker.date = habitTime
+        timePicker.backgroundColor = .white
+        timePicker.datePickerMode = .time
+        timePicker.preferredDatePickerStyle = .wheels
+        timePicker.addTarget(self, action: #selector(timeChanged), for: .valueChanged)
+        return timePicker
+    }()
+    
+    @objc func timeChanged(_ sender: UIDatePicker ){
+        habitTime = sender.date
+    }
+    
+    lazy var cancelButton: UIButton = {
+        let button = UIButton()
+        button.toAutoLayout()
+        button.setTitle("Отменить", for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 17, weight: .regular)
+        button.setTitleColor(UIColor(named: "purple"), for: .normal)
+        button.addTarget(self, action: #selector(cancelHabit), for: .touchUpInside)
+        return button
+    }()
+    
+    @objc func cancelHabit(){
+        dismiss(animated: true, completion: nil)
+    }
+    
+    lazy var saveButton: UIButton = {
+        let button = UIButton()
+        button.toAutoLayout()
+        button.setTitle("Сохранить", for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 17, weight: .regular)
+        button.setTitleColor(UIColor(named: "purple"), for: .normal)
+        button.addTarget(self, action: #selector(saveHabit), for: .touchUpInside)
+        return button
+    }()
+    
+    @objc func saveHabit(){
+        dismiss(animated: true, completion: nil)
+    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -89,6 +164,11 @@ class HabitVC: UIViewController {
         view.addSubview(habitNameText)
         view.addSubview(colorLabel)
         view.addSubview(colorButton)
+        view.addSubview(timeLabel)
+        view.addSubview(everyDayInLabel)
+        view.addSubview(setTimeValue)
+        view.addSubview(timePicker)
+        view.addSubview(cancelButton)
         
         useConstraint()
     
@@ -108,13 +188,35 @@ class HabitVC: UIViewController {
             colorLabel.topAnchor.constraint(equalTo: habitNameText.bottomAnchor, constant: 10),
             colorLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 15),
             
-            colorButton.topAnchor
-        
-        
+            colorButton.topAnchor.constraint(equalTo: colorLabel.bottomAnchor, constant: 10),
+            colorButton.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 15),
+            colorButton.heightAnchor.constraint(equalToConstant: 30),
+            colorButton.widthAnchor.constraint(equalToConstant: 30),
+            
+            timeLabel.topAnchor.constraint(equalTo: colorButton.bottomAnchor, constant: 10),
+            timeLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 15),
+            
+            everyDayInLabel.topAnchor.constraint(equalTo: timeLabel.bottomAnchor, constant: 10),
+            everyDayInLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 15),
+            
+            
+            setTimeValue.topAnchor.constraint(equalTo: timeLabel.bottomAnchor, constant: 10),
+            setTimeValue.leadingAnchor.constraint(equalTo: everyDayInLabel.trailingAnchor, constant: 5),
+            
+            
+            timePicker.topAnchor.constraint(equalTo: everyDayInLabel.bottomAnchor, constant: 10),
+            timePicker.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 15),
+            timePicker.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -15),
         
         ])
         
     }
     
 
+}
+
+extension HabitVC: UIColorPickerViewControllerDelegate, UITextFieldDelegate {
+    func colorPickerViewControllerDidFinish(_ viewController: UIColorPickerViewController) {
+        habitColor = viewController.selectedColor
+    }
 }
